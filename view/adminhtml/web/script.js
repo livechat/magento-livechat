9b8f19725e7387a06750ce89d5ee20012b777d9c
Paszczur <m.paszowski@livechatinc.com>
2018-01-31 12:46:39 +0100

    added new template for backend and accompanying assets

--- view/adminhtml/web/script.js
+++ view/adminhtml/web/script.js
@@ -1,198 +1,111 @@
-require(['jquery'], function($){
-    function LiveChatCreateNewAccount() {
-        var login = $('#lc_new_account_email').val();
-        var password = $('#lc_new_account_password').val();
-        var isError = false;
-        hideError('lc_new_account_email');
-        hideError('lc_new_account_password');
+require(['jquery'], function ($) {
+	
+	var save_props_url = $('#save-props-url').html();
+	var save_license_url = $('#save-license-url').html();
+	var reset_license_url = $('#reset-license-url').html();
+	var get_props_url = $('#get-props-url').html();
 
-        if (!login.length) {
-            showError('lc_new_account_email', 'Please enter your email address.');
-            isError = true;
-        }
+	var logoutLiveChat = function () {
+		sendMessage('logout');
+	};
 
-        if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i.test(login) === false) {
-            showError('lc_new_account_email', 'Please enter a valid email address.');
-            isError = true;
-        }
+	var login_with_livechat = document.getElementById('login-with-livechat');
 
-        if (password.length < 6) {
-            showError('lc_new_account_password', 'Password must be at least 6 characters.');
-            isError = true;
-        }
+	$('#lc_block_config_custom_params').on('change', function () {
+		var cart_products = $('#lc_block_config_custom_params_cart_products').val()
+		var total_cart_value = $('#lc_block_config_custom_params_total_cart_value').val();
+		var total_orders_count = $('#lc_block_config_custom_params_total_orders_count').val();
+		var last_order_details = $('#lc_block_config_custom_params_last_order_details').val();
+		
+		var param = {
+		cart_products : cart_products,	
+		total_cart_value : total_cart_value,	
+		total_orders_count : total_orders_count,	
+		last_order_details : last_order_details	
+		};
+		
+		$.ajax({
+			showLoader: true,
+			url: save_props_url,
+			data: param,
+			type: "POST",
+			dataType: 'json'
+		});
+	});
 
-        if (!password.length) {
-            showError('lc_new_account_password', 'Please choose a password.');
-            isError = true;
-        }
+	$('#reset_settings').on('click', function () {
+		
+		var param = {
+			param : 'param'		
+		};
+		
+		$.ajax({
+			showLoader: true,
+			type: 'POST',
+			dataType: 'json',
+			data: param,
+			url: reset_license_url
+		}).done(function () {
+			logoutLiveChat();
 
-        if (isError) {
-            return;
-        }
+			$('#login_panel').show();
+			$('#admin_panel').hide();
+			$('iframe#login-with-livechat').removeClass('hidden');
+			$('.progress-button').addClass('hidden');
+		});
 
-        var name = login.split("@")[0].split('+')[0];
+	});
 
-        $.ajax({
-            url: 'https://api.livechatinc.com/v2/license/',
-            data: {
-                name: name,
-                email: login,
-                password: password,
-                promo_code: 'magento2',
-                timezone: 'utc'
-            },
-            type: "POST",
-            dataType: 'json',
-            cache: false,
-            showLoader: true,
-            beforeSend: function (xhr) {
-                $('#lc_create_new_account_button').attr('disabled', 'disabled');
-            },
-            success: function (data, status, error) {
-                if (data.error) {
-                    showError('lc_new_account_password', data.error);
-                } else {
-                    $('#lc_block_config_account_license_id').val(data.license);
-                    $('#lc_block_config_account_license_email').val(login);
-                    $('#save').click();
-                }
-            },
-            error: function (data, status, error) {
-                var response = jQuery.parseJSON(data.responseText);
-                if (response.error) {
-                    if ('Agent already exists!' === response.error) {
-                        showError('lc_new_account_email', 'This email is already taken. Please try another or connect an existing account.');
-                    } else {
-                        showError('lc_new_account_password', response.error);
-                    }
-                } else if (response.errors) {
-                    showError('lc_new_account_password', response.errors[0]);
-                } else {
-                    showError('lc_new_account_password', 'Something went wrong. Please try again.');
-                }
-            },
-            complete: function (jqXHR, textStatus ) {
-                $('#lc_create_new_account_button').removeAttr('disabled');
-            }
-        });
-    }
+	var sendMessage = function (msg) {
+		login_with_livechat.contentWindow.postMessage(msg, '*');
+	};
 
-    function LiveChatExistingAccountForm() {
-        $('tr.lc_new_account').hide();
-        $('tr.lc_use_existing_account').show();
-        $('span.lc_new_account').hide();
-        $('span.lc_use_existing_account').show();
-    }
+	var logoutLiveChat = function () {
+		sendMessage('logout');
+	};
 
-    function LiveChatNewAccountForm() {
-        $('tr.lc_use_existing_account').hide();
-        $('tr.lc_new_account').show();
-        $('span.lc_use_existing_account').hide();
-        $('span.lc_new_account').show();
-    }
+	function receiveMessage(event) {
+		var livechatMessage = JSON.parse(event.data);
 
-    function LiveChatForm() {
-        var clickedElement = $('#lc_change_account');
-        if ($('#lc_form').hasClass('hidden')) {
-            clickedElement.html('Cancel');
-            $('#lc_form').removeClass('hidden');
-        } else {
-            clickedElement.html('Connect a different account');
-            $('#lc_form').addClass('hidden');
-        }
-    }
+		if (livechatMessage.type === 'logged-in' && livechatMessage.eventTrigger === 'click') {
 
-    function LiveChatConnect() {
-        var login = $('#lc_existing_account').val();
+			$('#login_panel').hide();
+			$('#admin_panel').show();
+			$('iframe#login-with-livechat').addClass('hidden');
+			$('.progress-button').removeClass('hidden');
+			
+			$.ajax({
+				showLoader: true,
+				type: 'POST',
+				dataType: 'json',
+				url: save_license_url,
+				data: {
+					email: livechatMessage.email,
+					license: livechatMessage.license
+				}
+			}).done(function(){
+				$.ajax({
+				showLoader: true,
+				type: 'GET',
+				url: get_props_url
+				}).done(function(result){
+					var license_settings = JSON.parse(result.license_settings);
+				
+					document.getElementById('livechat_login').innerHTML = 
+							license_settings.license_email;
+					document.getElementById('lc_block_config_custom_params_cart_products')
+							.value = Number(license_settings.cart_products);
+					document.getElementById('lc_block_config_custom_params_total_cart_value')
+							.value = Number(license_settings.total_cart_value);
+					document.getElementById('lc_block_config_custom_params_total_orders_count')
+							.value = Number(license_settings.total_orders_count);
+					document.getElementById('lc_block_config_custom_params_last_order_details')
+							.value = Number(license_settings.last_order_details);
+				});
+			});
+		}
+	}
 
-        if (!login.length) {
-            showError('lc_existing_account', 'Please enter your email address.');
-            return;
-        }
+	window.addEventListener("message", receiveMessage, false);
 
-        $.ajax({
-            url: 'https://api.livechatinc.com/licence/operator/' + login + '?callback=?',
-            data: {},
-            type: "GET",
-            dataType: 'jsonp',
-            cache: false,
-            showLoader: true,
-            beforeSend: function (xhr) {
-                hideError('lc_existing_account');
-                $('#lc_use_existing_account_button').attr('disabled', 'disabled');
-            },
-            success: function (data, status, error) {
-                if (data.error) {
-                    showError('lc_existing_account', 'Are you sure you entered a correct email address?');
-                } else if (data.number) {
-                    $('#lc_block_config_account_license_id').val(data.number);
-                    $('#lc_block_config_account_license_email').val(login);
-                    $('#save').click();
-                }
-            },
-            error: function (data, status, error) {
-                showError('lc_existing_account', 'Something went wrong. Please try again.');
-            },
-            complete: function (jqXHR, textStatus ) {
-                $('#lc_use_existing_account_button').removeAttr('disabled');
-            }
-        });
-    }
-
-    function showError(inputId, message) {
-        $('#' + inputId).addClass('mage-error');
-        $('#' + inputId + '-error').text(message).removeClass('lc-hidden').addClass('lc-show');
-    }
-
-    function hideError(inputId) {
-        $('#' + inputId).removeClass('mage-error');
-        $('#' + inputId + '-error').addClass('lc-hidden').removeClass('lc-show');
-    }
-
-    function checkAccount(licenseId) {
-        var statusContainer = $('#livechat-account-status');
-        $.ajax({
-            url: 'https://api.livechatinc.com/v2/license/' + licenseId + '?callback=?',
-            type: "GET",
-            dataType: 'jsonp',
-            cache: false,
-            beforeSend: function (xhr) {
-
-            },
-            success: function (data, status, error) {
-                if (data.error) {
-                    //
-                    statusContainer.text('Something went wrong, please reload page.')
-                } else {
-                    if (true === data.license_active) {
-                        statusContainer.text('Active.')
-                    } else {
-                        statusContainer.text('Inactive. Please login to LiveChat and subscribe')
-                    }
-                }
-            },
-            error: function (data, status, error) {
-                statusContainer.text('Something went wrong, please reload page.')
-            }
-        });
-    }
-
-    function disconnect() {
-        $('#lc_block_config_account_license_id').val(null);
-        $('#lc_block_config_account_license_email').val(null);
-        $('#save').click();
-    }
-
-    window.LiveChatConnect = LiveChatConnect;
-    window.LiveChatCreateNewAccount = LiveChatCreateNewAccount;
-    window.LiveChatForm = LiveChatForm;
-    window.LiveChatExistingAccountForm = LiveChatExistingAccountForm;
-    window.LiveChatNewAccountForm = LiveChatNewAccountForm;
-    window.LiveChatDisconnect = disconnect;
-
-    if ($('#livechat-account-status').length > 0) {
-        checkAccount($('#livechat-account-status').attr('data-license-id'));
-    }
-
-    window.setTimeout(function(){$('#config-edit-form').unbind('submit');},3000)
 });
