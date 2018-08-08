8840f772eccd29b9528266255c4402fc95d199f6
Michal Paszowski <m.paszowski@livechatinc.com>
2018-08-08 22:51:37 +0200

    removed 22 dependency

--- Controller/GetVisitor/Index.php
+++ Controller/GetVisitor/Index.php
@@ -1,28 +1,23 @@
 <?php
 namespace LiveChat\LiveChat\Controller\GetVisitor;
 
-use \LiveChat\LiveChat\Helper\Data;
-
 class Index extends \Magento\Framework\App\Action\Action
 {
     protected $_customerSession;
-    protected $_serializer;
     /**
      * @var UrlInterface
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\Serialize\Serializer\Json $serializer,
         \Magento\Customer\Model\Session $customerSession
     ){
         $this->_customerSession = $customerSession;
-        $this->_serializer = $serializer;
         return parent::__construct($context);
     } 
 
     public function execute()
     {
-        $visitor_data = 'var livechat_visitor_data = '.$this->_serializer->serialize($this->getCustomerDetails());
+        $visitor_data = 'var livechat_visitor_data = '. json_encode($this->getCustomerDetails());
     
         header('Content-type: application/javascript');
         echo $visitor_data;    
